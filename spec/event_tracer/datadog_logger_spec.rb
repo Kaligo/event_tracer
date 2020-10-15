e69d0e940d8045baa7b0baa15f2758d917d02efa
sylv3rblade <dev@sylv3rblade.com>
2020-10-15 08:36:53 +0800

    remove shared examples

--- spec/event_tracer/datadog_logger_spec.rb
+++ spec/event_tracer/datadog_logger_spec.rb
@@ -16,95 +16,90 @@ describe EventTracer::DatadogLogger do
 
   subject { EventTracer::DatadogLogger.new(mock_datadog) }
 
-  shared_examples_for 'rejects_invalid_datadog_args' do
-    INVALID_PAYLOADS.each do |datadog_value|
-      context "Invalid datadog top-level args" do
-        let(:datadog_payload) { datadog_value }
-
-        it 'rejects the payload when invalid datadog values are given' do
-          expect(mock_datadog).not_to receive(:increment)
-          expect(mock_datadog).not_to receive(:distribution)
-          expect(mock_datadog).not_to receive(:histogram)
-          expect(mock_datadog).not_to receive(:set)
-          expect(mock_datadog).not_to receive(:gauge)
-
-          result = subject.send(expected_call, datadog: datadog_payload)
-
-          expect(result.success?).to eq false
-          expect(result.error).to eq 'Invalid datadog config'
-        end
-      end
-    end
-  end
-
-  shared_examples_for 'skip_processing_empty_datadog_args' do
-    let(:datadog_payload) { {} }
-
-    it 'skips any metric processing' do
-      expect(mock_datadog).not_to receive(:increment_counter)
-      expect(mock_datadog).not_to receive(:add_distribution_value)
-      expect(mock_datadog).not_to receive(:set_gauge)
-
-      result = subject.send(expected_call, datadog: datadog_payload)
-
-      expect(result.success?).to eq true
-      expect(result.error).to eq nil
-    end
-  end
-
-  shared_examples_for 'processes_hashed_inputs' do
-    let(:datadog_payload) do
-      {
-        increment: { 'Counter_1' => 1, 'Counter_2' => 2 },
-        distribution: { 'Distribution_1' => 10 },
-        set: { 'Set_1' => 100 },
-        gauge: { 'Gauge_1' => 100 }
-      }
-    end
-
-    it 'processes each hash keyset as a metric iteration' do
-      expect(mock_datadog).to receive(:increment).with('Counter_1', 1)
-      expect(mock_datadog).to receive(:increment).with('Counter_2', 2)
-      expect(mock_datadog).to receive(:distribution).with('Distribution_1', 10)
-      expect(mock_datadog).to receive(:set).with('Set_1', 100)
-      expect(mock_datadog).to receive(:gauge).with('Gauge_1', 100)
-
-      result = subject.send(expected_call, datadog: datadog_payload)
-
-      expect(result.success?).to eq true
-      expect(result.error).to eq nil
-    end
-  end
-
-  shared_examples_for "rejects_invalid_metric_args" do
-    EventTracer::DatadogLogger::SUPPORTED_METRICS.each do |metric|
-      INVALID_PAYLOADS.each do |payload|
-        context "Invalid metric values for #{metric}: #{payload}" do
-          let(:datadog_payload) { { metric => payload } }
-
-          it 'rejects the payload when invalid datadog values are given' do
-            expect(mock_datadog).not_to receive(:increment_counter)
-            expect(mock_datadog).not_to receive(:add_distribution_value)
-            expect(mock_datadog).not_to receive(:set_gauge)
-
-            result = subject.send(expected_call, datadog: datadog_payload)
-
-            expect(result.success?).to eq false
-            expect(result.error).to eq "Datadog metric #{metric} invalid"
-          end
-        end
-      end
-    end
-  end
-
   EventTracer::LOG_TYPES.each do |log_type|
     context "Log type: #{log_type}" do
       let(:expected_call) { log_type }
 
-      it_behaves_like 'processes_hashed_inputs'
-      it_behaves_like 'skip_processing_empty_datadog_args'
-      it_behaves_like 'rejects_invalid_datadog_args'
-      it_behaves_like 'rejects_invalid_metric_args'
+      context 'processes_hashed_inputs' do
+        let(:datadog_payload) do
+          {
+            increment: { 'Counter_1' => 1, 'Counter_2' => 2 },
+            distribution: { 'Distribution_1' => 10 },
+            set: { 'Set_1' => 100 },
+            gauge: { 'Gauge_1' => 100 }
+          }
+        end
+
+        it 'processes each hash keyset as a metric iteration' do
+          expect(mock_datadog).to receive(:increment).with('Counter_1', 1)
+          expect(mock_datadog).to receive(:increment).with('Counter_2', 2)
+          expect(mock_datadog).to receive(:distribution).with('Distribution_1', 10)
+          expect(mock_datadog).to receive(:set).with('Set_1', 100)
+          expect(mock_datadog).to receive(:gauge).with('Gauge_1', 100)
+
+          result = subject.send(expected_call, datadog: datadog_payload)
+
+          expect(result.success?).to eq true
+          expect(result.error).to eq nil
+        end
+      end
+
+      context 'skip_processing_empty_datadog_args' do
+        let(:datadog_payload) { {} }
+
+        it 'skips any metric processing' do
+          expect(mock_datadog).not_to receive(:increment_counter)
+          expect(mock_datadog).not_to receive(:add_distribution_value)
+          expect(mock_datadog).not_to receive(:set_gauge)
+
+          result = subject.send(expected_call, datadog: datadog_payload)
+
+          expect(result.success?).to eq true
+          expect(result.error).to eq nil
+        end
+      end
+
+      context 'rejects_invalid_datadog_args' do
+        INVALID_PAYLOADS.each do |datadog_value|
+          context 'Invalid datadog top-level args' do
+            let(:datadog_payload) { datadog_value }
+
+            it 'rejects the payload when invalid datadog values are given' do
+              expect(mock_datadog).not_to receive(:increment)
+              expect(mock_datadog).not_to receive(:distribution)
+              expect(mock_datadog).not_to receive(:histogram)
+              expect(mock_datadog).not_to receive(:set)
+              expect(mock_datadog).not_to receive(:gauge)
+
+              result = subject.send(expected_call, datadog: datadog_payload)
+
+              expect(result.success?).to eq false
+              expect(result.error).to eq 'Invalid datadog config'
+            end
+          end
+        end
+      end
+
+      context 'rejects_invalid_metric_args' do
+        EventTracer::DatadogLogger::SUPPORTED_METRICS.each do |metric|
+          INVALID_PAYLOADS.each do |payload|
+            context "Invalid metric values for #{metric}: #{payload}" do
+              let(:datadog_payload) { { metric => payload } }
+
+              it 'rejects the payload when invalid datadog values are given' do
+                expect(mock_datadog).not_to receive(:increment_counter)
+                expect(mock_datadog).not_to receive(:add_distribution_value)
+                expect(mock_datadog).not_to receive(:set_gauge)
+
+                result = subject.send(expected_call, datadog: datadog_payload)
+
+                expect(result.success?).to eq false
+                expect(result.error).to eq "Datadog metric #{metric} invalid"
+              end
+            end
+          end
+        end
+      end
     end
   end
 end
