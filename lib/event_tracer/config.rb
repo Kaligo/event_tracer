2f153a77e1ecd84687729e9f5ace788157b9b6d8
hieuk09 <hieuk09@gmail.com>
2022-02-02 02:06:41 +0800

    Support dry-configurable 0.12 and earlier

--- lib/event_tracer/config.rb
+++ lib/event_tracer/config.rb
@@ -1,14 +1,24 @@
 require 'dry-configurable'
+require 'dry/configurable/version'
 
 module EventTracer
   class Config
     extend Dry::Configurable
 
-    setting :app_name, default: 'app_name'
+    if Dry::Configurable::VERSION >= "0.13"
+      setting :app_name, default: 'app_name'
 
-    # TODO: switch to namespace in v1.0
-    setting :dynamo_db_table_name, default: 'logs'
-    setting :dynamo_db_client
-    setting :dynamo_db_queue_name, default: 'low'
+      # TODO: switch to namespace in v1.0
+      setting :dynamo_db_table_name, default: 'logs'
+      setting :dynamo_db_client
+      setting :dynamo_db_queue_name, default: 'low'
+    else
+      setting :app_name, 'app_name'
+
+      # TODO: switch to namespace in v1.0
+      setting :dynamo_db_table_name, 'logs'
+      setting :dynamo_db_client
+      setting :dynamo_db_queue_name, 'low'
+    end
   end
 end
--- spec/data_helpers/mock_appsignal.rb
+++ spec/data_helpers/mock_appsignal.rb
@@ -1,4 +1,4 @@
-class MockAppsignal < Struct.new(:_)
+class MockAppsignal
   def increment_counter(*_args); 'increment_counter'; end
   def add_distribution_value(*_args); 'add_distribution_value'; end
   def set_gauge(*_args); 'set_gauge'; end
