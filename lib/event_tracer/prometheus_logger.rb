d9ad1ca750451e92c363c32a5b366b6d0e362f42
Tran Anh Vu <eyelight93@gmail.com>
2022-05-23 14:13:49 +0700

    Add PrometheusHelper module

--- lib/event_tracer/prometheus_logger.rb
+++ lib/event_tracer/prometheus_logger.rb
@@ -1,4 +1,16 @@
 module EventTracer
+  module PrometheusHelper
+    module_function
+
+    def register_prometheus_metrics(prometheus_registry, name, labels)
+      prometheus_registry.counter(
+        name.to_sym,
+        docstring: "A counter for #{name}",
+        labels: labels
+      )
+    end
+  end
+
   class PrometheusLogger < BasicDecorator
 
     SUPPORTED_METRIC_TYPES = {
