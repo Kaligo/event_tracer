3ed6a735d6dff0cb29576fc89b9815c7e6a3277a
Tran Anh Vu <eyelight93@gmail.com>
2022-05-24 10:42:30 +0700

    Update PrometheusHelper interface

--- lib/event_tracer/helpers/prometheus_helper.rb
+++ lib/event_tracer/helpers/prometheus_helper.rb
@@ -3,12 +3,22 @@ module EventTracer
     module PrometheusHelper
       module_function
 
-      def register_prometheus_metrics(prometheus_registry, name, labels)
-        prometheus_registry.counter(
-          name.to_sym,
-          docstring: "A counter for #{name}",
-          labels: labels
-        )
+      def register_prometheus_metrics(prometheus_registry, type, name, labels, aggregation: nil)
+        case type
+        when :counter
+          prometheus_registry.counter(
+            name.to_sym,
+            docstring: "A counter for #{name}",
+            labels: labels
+          )
+        when :gauge
+          prometheus_registry.gauge(
+            name.to_sym,
+            docstring: "A gauge for #{name}",
+            labels: labels,
+            store_settings: { aggregation: aggregation || :max }
+          )
+        end
       end
     end
   end
